339280db2969f2dea2784ff86bf5b74d4790debd
Fabricio <Fabricio@192.168.102.105>
2015-09-09 19:51:46 -0300

    Atualização da do formulario, incluindo campos com bootstrap.

--- usuarios/formulario.php
+++ usuarios/formulario.php
@@ -10,30 +10,46 @@
 <div class="jumbotron">
     <h2>Cadastro de Usuários</h2>
    <form action="processa.php?acao=salvar" method="post">
-<div class="input-group">
+<div class="form-group">
     <label class="control-label" for="email">E-mail</label>
     <input type="text" class="form-control" name="email" id="email" value="<?php echo $usuario['email'];?>">
 </div>
-       
-       <br>
-       Senha: <input type="text" name="senha" id="senha" value="<?php echo $usuario['senha'];?>">
-       <br>
-       Nome: <input type="text" name="nome" id="nome" value="<?php echo $usuario['nome'];?>">
-       <br>
-       CPF: <input type="text" name="cpf" id="cpf" value="<?php echo $usuario['cpf'];?>">
-       <br>
-       Endereço: <input type="text" name="endereco" id="endereco" value="<?php echo $usuario['endereco'];?>">
-       <br>
-       Número: <input type="text" name="numero" id="numero" value="<?php echo $usuario['numero'];?>">
-       <br>
-       Bairro: <input type="text" name="bairro" id="bairro" value="<?php echo $usuario['bairro'];?>">
-       <br>
-       Cidade: <input type="text" name="cidade" id="cidade" value="<?php echo $usuario['cidade'];?>">
-       <br>
-       Cep: <input type="text" name="cep" id="cep" value="<?php echo $usuario['cep'];?>">
-       <br>
-       Uf: <input type="text" name="uf" id="uf" value="<?php echo $usuario['uf'];?>">
-       <br>
+       <div class="form-group">
+           <label class="control-label" for="senha">Senha</label>
+           <input type="text" class="form-control" name="senha" id="senha" value="<?php echo $usuario['senha'];?>">
+       </div>
+       <div class="form-group">
+           <label class="control-label" for="nome">Nome</label>
+           <input type="text" class="form-control" name="nome" id="nome" value="<?php echo $usuario['nome'];?>">
+       </div>
+       <div class="form-group">
+           <label class="control-label" for="cpf">CPF</label>
+           <input type="text" class="form-control" name="cpf" id="cpf" value="<?php echo $usuario['cpf'];?>">
+       </div>
+       <div class="form-group">
+           <label class="control-label" for="endereco">Endereço</label>
+           <input type="text" class="form-control" name="endereco" id="endereco" value="<?php echo $usuario['endereco'];?>">
+       </div>
+       <div class="form-group">
+           <label class="control-label" for="numero">Número</label>
+           <input type="text" name="numero" id="numero" value="<?php echo $usuario['numero'];?>">
+       </div>
+       <div class="form-group">
+           <label class="control-label" for="bairro">Bairro</label>
+           <input type="text" name="bairro" id="bairro" value="<?php echo $usuario['bairro'];?>">
+       </div>
+       <div class="form-group">
+           <label class="control-label" for="cidade">Cidade</label>
+           <input type="text" name="cidade" id="cidade" value="<?php echo $usuario['cidade'];?>">
+       </div>
+       <div class="form-group">
+           <label class="control-label" for="cep">Cep</label>
+           <input type="text" name="cep" id="cep" value="<?php echo $usuario['cep'];?>">
+       </div>
+       <div class="form-group">
+           <label class="control-label" for="uf">Uf</label>
+           <input type="text" name="uf" id="uf" value="<?php echo $usuario['uf'];?>">
+       </div>
 
        <button type="submit">Enviar</button>
    </form>
